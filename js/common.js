015d93647101900e678c6489aa79d4f0b3bb9718
Saveliy <sava2698@gmail.com>
2023-03-23 21:53:07 +0600

    Reviews section created

--- js/common.js
+++ js/common.js
@@ -1,15 +1,27 @@
-$(function() {
+$(function () {
+  new WOW().init();
 
-	new WOW().init();
+  $('.hamburger').click(function () {
+    $(this).toggleClass('is-active');
 
-	$(".hamburger").click(function(){
-		$(this).toggleClass("is-active");
+    if ($(this).hasClass('is-active')) {
+      $('.mnu_top').slideDown(300);
+    } else {
+      $('.mnu_top').slideUp(300);
+    }
+  });
 
-		if($(this).hasClass('is-active')){
-			$('.mnu_top').slideDown(300);
-		}else{
-			$('.mnu_top').slideUp(300);
-		}
-	});
+  /*--------REVIEWS TABS--------*/
 
+  $('.nav_rev > div').click(function () {
+    const revId = $(this).data('revid');
+
+    $('.nav_rev > div').not(this).removeClass('active');
+    $(this).addClass('active');
+
+    $('.item_rev').not(revId).removeClass('active');
+    $(revId).addClass('active');
+  });
+
+  /*----------------------------*/
 });
